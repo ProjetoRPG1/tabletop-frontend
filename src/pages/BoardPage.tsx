3a3ae0416dbcc27d1a3176bc99af4a593107ab12
Eduardo P. Duarte <eduardo.possa.duarte@sap.com>
2025-10-07 15:16:04 -0300

    feat: adding token component with drag and drop behavior

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -2,18 +2,25 @@ import { useParams } from "react-router-dom";
 import CustomButton from "../components/Button";
 import CustomInput from "../components/Input";
 import Board from "../components/Board";
+import { Token } from "../components/Token";
 
 const BoardPage = () => {
   const { id } = useParams();
 
   return (
     <div>
-      <Board image="https://picsum.photos/3000/3000"></Board>
+      <Board image="https://picsum.photos/1440/1080"></Board>
       <h1>Board {id}</h1>
       <CustomButton onClick={() => {}}>
         <span>Teste button</span>
       </CustomButton>
       <CustomInput label="Teste input" variant={"outlined"} />
+
+      <Token
+        image="https://fastly.picsum.photos/100/100"
+        description="test"
+      />
+
     </div>
   );
 }
